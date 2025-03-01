6709f3561b7fc549448970bc09b3d695dde6701f
karankumaryadav <kkcgamer865@gmail.com>
2025-03-01 11:53:30 +0530

    change-2

--- client/src/redux/slices/itemSlice.js
+++ client/src/redux/slices/itemSlice.js
@@ -2,6 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 // const API_URL = 'http://localhost:5000/api/items';
+// const API_URL = `${import.meta.env.VITE_API_URL}/items`;
 const API_URL = `${import.meta.env.VITE_API_URL}/items`;
 
 // Get all items
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,15 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist", // Ensure Vercel picks up the correct directory
-    emptyOutDir: true, // Clean the output directory before building
-  },
-  server: {
-    port: 5173, // Default Vite port (adjust if necessary)
-    strictPort: true,
-  },
   define: {
-    "process.env": {}, // Prevent "process is not defined" errors in Vite
+    "process.env": {},
   },
 });
